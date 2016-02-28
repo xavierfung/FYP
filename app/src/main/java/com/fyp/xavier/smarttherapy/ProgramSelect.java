08cbd43f0be940c47600119b8ff90aadf1dd843b
xavier <xavierfung123@yahoo.com.hk>
2016-02-29 04:33:30 +0800

    register

--- app/src/main/java/com/fyp/xavier/smarttherapy/ProgramSelect.java
+++ app/src/main/java/com/fyp/xavier/smarttherapy/ProgramSelect.java
@@ -5,6 +5,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.TextView;
+
+import com.fyp.xavier.smarttherapy.helper.SQLiteHandler;
+import com.fyp.xavier.smarttherapy.helper.SessionManager;
+
+import java.util.HashMap;
 
 /**
  * Created by Xavier on 16/1/2016.
@@ -12,6 +18,10 @@ import android.widget.ImageButton;
 public class ProgramSelect extends Activity {
     private ImageButton iButton1;
     private ImageButton iButton2;
+    private TextView twname, twplace;
+    private SQLiteHandler db;
+    private SessionManager session;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -20,6 +30,29 @@ public class ProgramSelect extends Activity {
         setContentView(R.layout.program_select);
         iButton1 = (ImageButton) findViewById(R.id.imageButton1);
         iButton2 = (ImageButton) findViewById(R.id.imageButton2);
+        twname = (TextView) findViewById(R.id.name);
+        twname = (TextView) findViewById(R.id.place);
+
+        // SqLite database handler
+        db = new SQLiteHandler(getApplicationContext());
+
+        // session manager
+        session = new SessionManager(getApplicationContext());
+/**
+ if (!session.isLoggedIn()) {
+ logoutUser();
+ }
+ **/
+        // Fetching user details from SQLite
+        HashMap<String, String> user = db.getUserDetails();
+
+        String name = user.get("name");
+        String email = user.get("email");
+
+        // Displaying the user details on the screen
+        twname.setText(name);
+        twplace.setText(email);
+
 
         iButton1.setOnClickListener(new View.OnClickListener() {
             @Override
