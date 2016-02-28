235da212168e94473a78948138825aec34deea26
xavier <xavierfung123@yahoo.com.hk>
2016-02-29 02:48:49 +0800

    adding login function

--- app/src/main/java/com/fyp/xavier/smarttherapy/LoginActivity.java
+++ app/src/main/java/com/fyp/xavier/smarttherapy/LoginActivity.java
@@ -1,9 +1,13 @@
 package com.fyp.xavier.smarttherapy;
 
 import android.app.Activity;
+import android.app.Dialog;
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageButton;
 
 /**
@@ -11,14 +15,26 @@ import android.widget.ImageButton;
  */
 public class LoginActivity extends Activity {
 
+    private EditText editTextUserName;
+    private EditText editTextPassword;
     private ImageButton LoginBtn;
+
+    public static final String USER_NAME = "USERNAME";
+
+    String username;
+    String password;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
     /**/
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
-        LoginBtn = (ImageButton) findViewById(R.id.imageButton1);
+        editTextUserName = (EditText) findViewById(R.id.username);
+        editTextPassword = (EditText) findViewById(R.id.password);
+
+
+    LoginBtn = (ImageButton) findViewById(R.id.imageButton1);
 
         LoginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
