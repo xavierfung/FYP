637020647e8a0bef4ae422428182ad9d4f7104ef
xavier <xavierfung123@yahoo.com.hk>
2016-03-04 12:12:43 +0800

    server to openshift

--- app/src/main/java/com/fyp/xavier/smarttherapy/app/AppConfig.java
+++ app/src/main/java/com/fyp/xavier/smarttherapy/app/AppConfig.java
@@ -2,14 +2,14 @@ package com.fyp.xavier.smarttherapy.app;
 
 public class AppConfig {
     // Server user login url
-    public static String URL_LOGIN = "http://192.168.1.101/android_login_api/login.php";
+    public static String URL_LOGIN = "http://php-1155033392.rhcloud.com/android_login_api/login.php";
 
     // Server user register url
-    public static String URL_REGISTER = "http://192.168.1.101/android_login_api/register.php";
+    public static String URL_REGISTER = "http://php-1155033392.rhcloud.com/android_login_api/register.php";
 
     // Server user register url
-    public static String URL_UPLOAD = "http://192.168.1.101/android_score_api/create_record.php";
+    public static String URL_UPLOAD = "http://php-1155033392.rhcloud.com/android_score_api/create_record.php";
 
     // Server user register url
-    public static String URL_GET = "http://192.168.1.101/android_score_api/get_all_recordx.php";
+    public static String URL_GET = "http://php-1155033392.rhcloud.com/android_score_api/get_all_recordx.php";
 }
